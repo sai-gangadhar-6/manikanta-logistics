12ad1de5970ee9525c8ce7ef09b36fc97425af7e
Manikanta Logistics <movers@manikanta.com>
2026-07-06 17:50:05 +0530

    ui: remove GPS/PanIndia/DamageFree cards, add icon connector line, compact Why Choose Us, remove stats banner section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Services from '@/components/Services'
 import WhyUs from '@/components/WhyUs'
-import TrustStats from '@/components/TrustStats'
 import Process from '@/components/Process'
 import Pricing from '@/components/Pricing'
 import Testimonials from '@/components/Testimonials'
@@ -23,7 +22,6 @@ export default function HomePage() {
         <About />
         <Services />
         <WhyUs />
-        <TrustStats />
         <Process />
         <Pricing />
         <Testimonials />
